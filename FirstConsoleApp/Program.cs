53c3152186177d2e7eaae323741c8dc71c82ef21
karim Salaheldeen <eng.karimsalaheldeen@gmail.com>
2026-05-17 11:10:05 +0400

    implement print from 1 to 10

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1-. Write a C# program that prints from 1 to 10
+            int counter;
+
+            while (true)
+            {
+
+                for (counter = 1; counter <= 10; counter++)
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+
+         
+
+
+
+
+
         }
     }
 }

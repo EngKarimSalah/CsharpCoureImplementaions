a8909e9f174b9dc5c530f10c9d7e077c22a53526
karim Salaheldeen <eng.karimsalaheldeen@gmail.com>
2026-05-17 11:13:16 +0400

    implement welcome message

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,20 +7,17 @@
             //1-. Write a C# program that prints from 1 to 10
             int counter;
 
-            while (true)
-            {
-
-                for (counter = 1; counter <= 10; counter++)
+            for (counter = 1; counter <= 10; counter++)
                 {
                     Console.WriteLine(counter);
                 }
-            }
-
-         
 
 
-
-
+            //2- print welcome message to the user and ask for their name
+            Console.WriteLine("Welcome to the application!");
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello" + userName);
 
         }
     }
